6d9b91083859788e77a56634931589112caf7044
draguntsow <ranger_roman@mail.ru>
2018-07-28 16:40:50 +0300

    Create entry.py

---
+++ schaden/entry.py
@@ -0,0 +1,11 @@
+class application(object):
+
+    def __init__(self, environ, start_response):
+        self.environ = environ
+        self.start = start_response
+
+    def __iter__(self):
+        status = '200 OK'
+        response_headers = [('Content-type', 'text/plain')]
+        self.start(status, response_headers)
+        yield "TEST\n"
