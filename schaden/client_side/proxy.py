66bc681aad51771b21ee58d908dc5c4aec4e329d
Draguntsow <draguntsow@yahoo.com>
2018-07-31 13:48:45 +0300

    OOM

--- schaden/client_side/proxy.py
+++ schaden/client_side/proxy.py
@@ -5,15 +5,17 @@ import Crypto
 
 class SchadenClient(object):
     def __init__(self):
-        self.Cryptor = CryptoProvider()
-        self.Holder = ExHandler()
-        self.Coordinator = Coordinator()
-        self.ConnectionManager = ConnectionManager()
-        self.GateFabric = GateFabric()
-        self.Inner_Gate = Gate()
+        self.cryptor = CryptoProvider()
+        self.holder = ExHandler()
+        self.coordinator = Coordinator()
+        self.connection_manager = ConnectionManager()
+        self.inner_gate = Gate()
 
     def run(self):
-        #TODO
+        self.inner_gate.open()
+
+        while True:
+
         return 0
 
     def stop(self):
@@ -36,30 +38,34 @@ class _ConnectionManager(object):
         self.connection_list = []
 
 class Gate(object):
-    def __init__(self, port = 50600, error_handler=None):
-        self.addr = ('127.0.0.1', port)
-        self._host_conn = ''
-        self._host_addr = ''
-        self.data = bytes()
-
-        self.gateway = socket.socket(family=socket.AF_INET,
-                                     type=socket.SOCK_STREAM)
-        self.gateway.bind(self.addr)
+    def __init__(self, error_handler=None):
+        self.error = error_handler
 
     def open(self):
+        self.gateway.bind(self.addr)
         while True:
             self.gateway.listen(5)
     
             self._host_conn, self._host_addr = self.gateway.accept()
+            self.data = self._host_conn.recv(2048)
+            self.data = self.data.decode('ascii')
 
-            #self.data = self._host_conn.recv(2048)
-            #self.data = self.data.decode('ascii')
-            #print(self.data)
         return 0
 
+class InnerGate(Gate):
+    def __init__(self, port = 50600, error_handler=None):
+        super().__init__(error_handler)
+        self.received_conn = ''
+        self.received_addr = ''
+        self.addr = ('127.0.0.1', port)
+        self.data = bytes()
+
+        self.gateway = socket.socket(family=socket.AF_INET,
+                                     type=socket.SOCK_STREAM)
 
 
 if __name__ == '__main__':
-
+    T_input = Gate()
+    T_input.open()
     Main = SchadenClient.run()
 
